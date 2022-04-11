f940d00f2028d9d9eb58904855dfbe2b87238e78
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-04-11 01:49:35 +0100

    add required components

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,18 +1,146 @@
-import { useRouter } from "next/router";
-import BackLink from "../components/BackLink";
-import PageHeading from "../components/PageHeading";
-import calculatePrice from "../lib/calculatePrice";
+import { useRouter } from 'next/router'
+import { useConnection, useWallet } from '@solana/wallet-adapter-react'
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+import { Keypair, Transaction } from '@solana/web3.js'
+import { useState, useEffect, useMemo } from 'react'
+import BackLink from '../components/BackLink'
+import Loading from '../components/Loading'
+import {
+  MakeTransactionInputData,
+  MakeTransactionOutputData,
+} from './api/makeTransaction'
+import {
+  findTransactionSignature,
+  FindTransactionSignatureError,
+} from '@solana/pay'
 
 export default function Checkout() {
   const router = useRouter()
+  const { connection } = useConnection()
+  const { publicKey, sendTransaction } = useWallet()
 
-  const amount = calculatePrice(router.query)
+  const [transaction, setTransaction] = useState<Transaction | null>(null)
+  const [message, setMessage] = useState<string | null>(null)
+
+  const searchParams = new URLSearchParams()
+  for (const [key, value] of Object.entries(router.query)) {
+    if (value) {
+      if (Array.isArray(value)) {
+        for (const v of value) {
+          searchParams.append(key, v)
+        }
+      } else {
+        searchParams.append(key, value)
+      }
+    }
+  }
+
+  const reference = useMemo(() => Keypair.generate().publicKey, [])
+
+  searchParams.append('reference', reference.toString())
+
+  async function getTransaction() {
+    if (!publicKey) {
+      return
+    }
+
+    const body: MakeTransactionInputData = {
+      account: publicKey.toString(),
+    }
+
+    const response = await fetch(
+      `/api/makeTransaction?${searchParams.toString()}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      }
+    )
+
+    const json = (await response.json()) as MakeTransactionOutputData
+
+    if (response.status !== 200) {
+      console.error(json)
+      return
+    }
+
+    const transaction = Transaction.from(
+      Buffer.from(json.transaction, 'base64')
+    )
+    setTransaction(transaction)
+    setMessage(json.message)
+    console.log(transaction)
+  }
+
+  useEffect(() => {
+    getTransaction()
+  }, [publicKey])
+
+  async function trySendTransaction() {
+    if (!transaction) {
+      return
+    }
+
+    try {
+      await sendTransaction(transaction, connection)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  useEffect(() => {
+    trySendTransaction()
+  }, [transaction])
+
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      try {
+        const signatureInfo = await findTransactionSignature(
+          connection,
+          reference
+        )
+        router.push('/confirmed')
+      } catch (e) {
+        if (e instanceof FindTransactionSignatureError) {
+          return
+        }
+        console.error('An error was found', e)
+      }
+    }, 500)
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
+
+  if (!publicKey) {
+    return (
+      <div className="flex flex-col items-center gap-8">
+        <BackLink href="/">Cancel</BackLink>
+
+        <WalletMultiButton />
+
+        <p>You need to connect your wallet to make transactions</p>
+      </div>
+    )
+  }
 
   return (
-    <div className="flex flex-col gap-8 items-center">
-      <BackLink href='/'>Cancel</BackLink>
+    <div className="flex flex-col items-center gap-8">
+      <div>
+        <BackLink href="/">Cancel</BackLink>
+      </div>
 
-      <PageHeading>Checkout {amount.toString()} SOL</PageHeading>
+      <WalletMultiButton />
+
+      {message ? (
+        <p>{message} Please approve the transaction using your wallet </p>
+      ) : (
+        <p>
+          Creating transaction ... <Loading />
+        </p>
+      )}
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,22 @@
 import Products from '../components/Products'
 import SiteHeading from '../components/SiteHeading'
+import { useWallet } from '@solana/wallet-adapter-react'
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+import CouponBook from '../components/CouponBook'
 
 export default function HomePage() {
+  const { publicKey } = useWallet()
   return (
-    <div className="flex flex-col gap-8 max-w-4xl items-stretch m-auto pt-24">
-      <SiteHeading>Cookies Inc</SiteHeading>
+    <div className="m-auto flex max-w-4xl flex-col items-stretch gap-8 pt-24">
+      <SiteHeading>Jollof Rice inc</SiteHeading>
 
-      <Products submitTarget='/checkout' enabled={true} />
+      <div className="basis-1/4">
+        <WalletMultiButton className="!bg-gray-900 hover:scale-105" />
+      </div>
+
+      {publicKey && <CouponBook />}
+
+      <Products submitTarget="/checkout" enabled={publicKey !== null} />
     </div>
   )
 }
