e685db88c702c46b81f44543dedb92bd393e9e25
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-04-11 01:50:57 +0100

    add necessary libraries files

---
+++ lib/addresses.ts
@@ -0,0 +1,13 @@
+import { PublicKey } from '@solana/web3.js'
+
+export const shopAddress = new PublicKey(
+  '9LzqPteKHceaFcC9FCMGjQmUoKppZTVS99RHfMviEbPs'
+)
+
+export const usdcAddress = new PublicKey(
+  'Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr'
+)
+
+export const couponAddress = new PublicKey(
+  'BmpZVQ5n1cPQHmMZfh4HdQiEDszo6kPeSnKgVudqkJez'
+)
--- lib/products.ts
+++ lib/products.ts
@@ -1,18 +1,18 @@
 export const products = [
   {
-    id: 'box-of-cookies',
-    name: 'Box',
-    description: 'A delicious box of handmade cookies',
+    id: 'box-of-rice',
+    name: 'Pack',
+    description: 'A delicious pack of Jollof rice and Chicken',
     unitName: 'box', // shows after the price, eg. 0.05 SOL/box
     priceSol: 0.05,
     priceUsd: 5,
   },
   {
-    id: 'basket-of-cookies',
+    id: 'basket-of-rice',
     name: 'Basket',
-    description: 'A large basket of handmade cookies',
+    description: 'A large basket of Jollof rice and Chicken',
     unitName: 'basket',
     priceSol: 0.1,
     priceUsd: 10,
-  }
+  },
 ]
